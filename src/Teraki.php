3be664101f3e3d5b73e0d6135c12b08ffae9ee16
tinsaebelay <tinsaedev@gmail.com>
2022-01-22 02:11:35 +0300

    scafollding 1

---
+++ src/Teraki.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace Sazx\SazxTeraki;
+
+
+    class Teraki {
+
+        function __construct(){
+
+        }
+
+
+        public static function sayHello(){
+
+            return " Greetings from Teraki ";
+        }
+    }
+
+?>
+
