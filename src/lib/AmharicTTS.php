753a7aaaf1932619916c8f48d54f31a90892607c
tinsaebelay <tinsaedev@gmail.com>
2022-01-22 03:00:52 +0300

    lib added

---
+++ src/lib/AmharicTTS.php
@@ -0,0 +1,15 @@
+<?php
+    namespace Sazx\SazxTeraki\Lib;
+
+
+    class AmharicTTS{
+
+        public static function who(){
+            
+            ?>
+                <h1> I AmharicTTS , fron lib package   </h1>
+            <?php
+        }
+    }
+
+?>
